ba079a205653935d3ee8c99458fc4bfce96322c5
priyajit <iampd10official@gmail.com>
2025-07-19 13:23:02 +0530

    register done

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv/config'
 import { Connection } from './src/config/dbConnection.js';
+import route from './src/routes/userRoute.js';
 
 const app = express();
 
 const port= process.env.PORT;
 
 Connection()
+app.use(express.json())
+app.use('/user', route)
 
 app.listen(port,()=>{
     console.log(`server running at port ${port}`);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,41 @@
-const a = 100
+import dotenv from 'dotenv/config';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import userSchema from '../models/userSchema.js';
+import { verifyEmail } from '../verifyEmail/verifyEmail.js';
+
+export const register = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    console.log("test",userName, email, password)
+    const existing = await userSchema.findOne({ email: email });
+    if (existing) {
+      return res.status(401).json({
+        success: false,
+        message: "User Already Exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await userSchema.create({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+    const token = jwt.sign({ id: user._id }, process.env.secretKey, {
+      expiresIn: "15m",
+    });
+    verifyEmail(token, email);
+    user.token = token;
+    await user.save();
+    return res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      data: { userName: userName },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
